c72df3afff9cfa9ef97f8924ce185f37dd19f7ee
Brady <cassadab@msoe.edu>
2020-03-24 19:46:26 -0500

    cleaned up styling and removed some old code

--- src/components/io_reg.rs
+++ src/components/io_reg.rs
@@ -13,7 +13,6 @@ pub struct IORegisters {
     updated_reg_dec: String,
     update_reg_num: u8,
     link: ComponentLink<Self>,
-    tab_states: TabStates
 }
 
 pub enum Msg {
@@ -21,23 +20,6 @@ pub enum Msg {
     UpdateReg(String, u8, RegUpdateType),
     FinishUpdate(RegUpdateType),
     Nope
-}
-
-#[derive(PartialEq, Clone, Copy)]
-pub enum TabState {
-    Closed,
-    Open,
-}
-
-pub struct TabStates {
-    lcd: TabState,
-    sound: TabState,
-    dma: TabState,
-    timer: TabState,
-    sc1: TabState,
-    keypad: TabState,
-    sc2: TabState,
-    int_ws_pd: TabState,
 }
 
 impl Component for IORegisters {
@@ -51,16 +33,6 @@ impl Component for IORegisters {
             updated_reg_hex: "".to_string(),
             update_reg_num: 0,
             link: link,
-            tab_states: TabStates {
-                lcd: TabState::Closed,
-                sound: TabState::Closed,
-                dma: TabState::Closed,
-                timer: TabState::Closed,
-                sc1: TabState::Closed,
-                keypad: TabState::Closed,
-                sc2: TabState::Closed,
-                int_ws_pd: TabState::Closed
-            }
         }
     }
 
@@ -129,25 +101,25 @@ impl Component for IORegisters {
         html! {
             <div>
                 <div id="accordion">
-                  <div class="card">
+                  <div class="card border-0">
                     <div class="card-header p-0 bg-transparent" id="headingOne">
                       <h5 class="mb-0">
-                        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        <button class="btn btn-link text-dark" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                           {"LCD"}
                         </button>
                       </h5>
                     </div>
 
-                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne">
+                    <div id="collapseOne" class="collapse" aria-labelledby="headingOne">
                       <div class="card-body">
                           {self.view_lcd()}
                       </div>
                     </div>
                   </div>
-                  <div class="card">
+                  <div class="card border-0">
                     <div class="card-header p-0 bg-transparent" id="headingTwo">
                       <h5 class="mb-0">
-                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        <button class="btn btn-link text-dark collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                           {"Sound"}
                         </button>
                       </h5>
@@ -158,10 +130,10 @@ impl Component for IORegisters {
                       </div>
                     </div>
                   </div>
-                  <div class="card">
+                  <div class="card border-0">
                     <div class="card-header p-0 bg-transparent" id="headingThree">
                       <h5 class="mb-0">
-                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        <button class="btn btn-link text-dark collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                           {"DMA"}
                         </button>
                       </h5>
@@ -172,10 +144,10 @@ impl Component for IORegisters {
                       </div>
                     </div>
                   </div>
-                  <div class="card">
+                  <div class="card border-0">
                     <div class="card-header p-0 bg-transparent" id="headingFour">
                       <h5 class="mb-0">
-                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                        <button class="btn btn-link text-dark collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
                           {"Timer"}
                         </button>
                       </h5>
@@ -186,10 +158,10 @@ impl Component for IORegisters {
                       </div>
                     </div>
                   </div>
-                  <div class="card">
+                  <div class="card border-0">
                     <div class="card-header p-0 bg-transparent" id="headingFive">
                       <h5 class="mb-0">
-                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                        <button class="btn btn-link text-dark collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
                           {"Serial Communication 1"}
                         </button>
                       </h5>
@@ -200,10 +172,10 @@ impl Component for IORegisters {
                       </div>
                     </div>
                   </div>
-                  <div class="card">
+                  <div class="card border-0">
                     <div class="card-header p-0 bg-transparent" id="headingSix">
                       <h5 class="mb-0">
-                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                        <button class="btn btn-link text-dark collapsed" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
                           {"Keypad Input"}
                         </button>
                       </h5>
@@ -214,10 +186,10 @@ impl Component for IORegisters {
                       </div>
                     </div>
                   </div>
-                  <div class="card">
+                  <div class="card border-0">
                     <div class="card-header p-0 bg-transparent" id="headingSeven">
                       <h5 class="mb-0">
-                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                        <button class="btn btn-link text-dark collapsed" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
                           {"Serial Communication 2"}
                         </button>
                       </h5>
@@ -228,10 +200,10 @@ impl Component for IORegisters {
                       </div>
                     </div>
                   </div>
-                  <div class="card">
+                  <div class="card border-0">
                     <div class="card-header p-0 bg-transparent" id="headingEight">
                       <h5 class="mb-0">
-                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+                        <button class="btn btn-link text-dark collapsed" data-toggle="collapse" data-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
                           {"Interrupt, Waitstate, and Power-Down"}
                         </button>
                       </h5>
