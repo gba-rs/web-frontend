0b6078d8958eee67fe2fd0c2b2338730a68e2fa9
Noe Gonzalez <gnoe234@gmail.com>
2026-09-02 12:39:15 -0700

    Rebuild pkg with the performance fixes

--- pkg/bundle.js
+++ pkg/bundle.js
@@ -705,12 +705,12 @@
                 return ret;
             },
             __wbindgen_cast_0000000000000003: function(arg0, arg1) {
-                // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [NamedExternref("IDBVersionChangeEvent")], shim_idx: 483, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+                // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [NamedExternref("IDBVersionChangeEvent")], shim_idx: 324, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
                 const ret = makeMutClosure(arg0, arg1, wasm_bindgen_f7b093aeba8fc579___convert__closures_____invoke___web_sys_c3bf51ed8a6154dc___features__gen_KeyboardEvent__KeyboardEvent______true_);
                 return ret;
             },
             __wbindgen_cast_0000000000000004: function(arg0, arg1) {
-                // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [NamedExternref("KeyboardEvent")], shim_idx: 483, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+                // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [NamedExternref("KeyboardEvent")], shim_idx: 324, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
                 const ret = makeMutClosure(arg0, arg1, wasm_bindgen_f7b093aeba8fc579___convert__closures_____invoke___web_sys_c3bf51ed8a6154dc___features__gen_KeyboardEvent__KeyboardEvent______true__3);
                 return ret;
             },
